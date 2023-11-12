c5752990a30aa7a6633cd8147aa4e7fc82318fcf
klliio <dylantobiasphilp@gmail.com>
2023-11-12 23:55:50 +0000

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use clap::Parser;
+
+/// A static bar builder
+#[derive(Parser, Debug)]
+#[command(author = "klliio", version, about, long_about = None, arg_required_else_help = true)]
+struct Args {
+    // Current fill progress
+    #[arg(short, long)]
+    progress: u16,
+
+    // Maximum value
+    #[arg(short, long)]
+    max: u16,
+
+    // Length of the bar
+    #[arg(short, long)]
+    length: u16,
+
+    // Character to show filled progress
+    #[arg(long, default_value_t = '█')]
+    fill_char: char,
+
+    // Charater to show empty bar
+    #[arg(long, default_value_t = '▓')]
+    empty_char: char,
+}
+
+fn main() {
+    let args = Args::parse();
+    let value_percent: u16;
+
+    // lower <max> to 100 if possible to reduce the need to use floating points
+    if (args.max > 100) && (args.progress > (args.max - 100)) {
+        value_percent = args.progress - (args.max - 100);
+    } else {
+        // using 'as' as it allows for conversion to u16 and cannot panic here
+        value_percent = ((100.0 * f32::from(args.progress)) / f32::from(args.max)) as u16;
+    }
+
+    let fill_amount: u16 = ((f32::from(args.length) / 100.0) * f32::from(value_percent)) as u16;
+
+    let mut i: u16 = 0;
+    let mut bar: String = String::new();
+    while i < fill_amount {
+        bar.push(args.fill_char);
+        i += 1;
+    }
+    while i < args.length {
+        bar.push(args.empty_char);
+        i += 1;
+    }
+
+    println!("{}", bar);
+}
